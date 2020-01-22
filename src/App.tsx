fb47feccf1e147c04e7ae738491303b2f9cac6e0
shiyuetc <shiyuetc@users.noreply.github.com>
2020-01-22 20:55:09 +0900

    Fix title

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <div className="Content">
-        <h2 className="Title">国旗クイズ</h2>
+        <h2 className="Title">国旗当てクイズ</h2>
         <Quiz countries={Countries}/>
       </div>
     </div>
