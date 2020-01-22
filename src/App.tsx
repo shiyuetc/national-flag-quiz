b4f69931ea66423456519c2eda94157aa0ae3bca
shiyuetc <shiyuetc@users.noreply.github.com>
2020-01-22 20:45:43 +0900

    Add App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Quiz from './components/Quiz';
+import { Countries } from './data/Countries';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      
+      <div className="Content">
+        <h2 className="Title">国旗クイズ</h2>
+        <Quiz countries={Countries}/>
+      </div>
     </div>
   );
 }
