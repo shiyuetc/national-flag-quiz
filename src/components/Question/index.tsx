7520d108c9e20b276c97af7eeb025ec0ffbd3ee7
shiyuetc <shiyuetc@users.noreply.github.com>
2020-01-22 19:08:39 +0900

    Add Question component

---
+++ src/components/Question/index.tsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import AnswerButton from '../AnswerButton';
+import MessageText from '../MessageText';
+import { Country } from '../../interfaces/Country';
+
+interface Props {
+  enable: boolean,
+  answer: number,
+  candidates: Array<Country>,
+  answerResponse: Function
+}
+
+interface States {
+  isRight?: boolean
+}
+
+class Question extends Component<Props, States> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { isRight: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(code: string) {
+    if(this.props.enable) {
+      var is_right = this.props.candidates[this.props.answer].code === code;
+      this.setState({ isRight: is_right });
+      this.props.answerResponse(is_right);
+    }
+  }
+
+  render() {
+    const alpha = ['A', 'B', 'C', 'D'];
+    return (
+      <div>
+        <img src={'images/nf/' + this.props.candidates[this.props.answer].english + '.png'} alt="flag" />
+        <MessageText answerText={this.props.candidates[this.props.answer].japanese} isAnswered={!this.props.enable} isRight={this.state.isRight} />
+        {this.props.candidates.map((v, index) => (
+          <AnswerButton key={index} code={v.code} buttonText={alpha[index]} outerText={v.japanese} handleClick={this.handleClick} disabled={!this.props.enable} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Question;
